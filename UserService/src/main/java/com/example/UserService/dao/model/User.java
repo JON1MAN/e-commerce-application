7fd3c0cd0cba3bf4d1c2deaf63bde2b90c9f4923
aliaksei <aliaksmarchuk@gmail.com>
2024-12-11 02:04:01 +0100

    user entity with migration

---
+++ UserService/src/main/java/com/example/UserService/dao/model/User.java
@@ -0,0 +1,27 @@
+package com.example.UserService.dao.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+}
